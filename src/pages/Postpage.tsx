c7f2f3b2f5e16c471a6fd68056f4ee2417b0cd66
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-04-30 15:58:35 -0400

    properly passes props

--- src/pages/Postpage.tsx
+++ src/pages/Postpage.tsx
@@ -5,10 +5,9 @@ import Post from '../components/blog/Post'
 class Postpage extends Component<any, any> {
 
     render() {
-        const { match } = this.props
         return (
             <div>
-                <Post post={match.params.slug} />
+                <Post post={this.props.match.params.slug} />
             </div>
         )
     }
