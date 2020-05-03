a869ad2396135c6a81cdb630929ebdf228c394ae
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-02 20:11:23 -0400

    aligning elements in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom'
 import styles from './Header.module.scss'
 
 import ThemeSwitch from './ThemeSwitch'
+import Logo from './Logo'
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <nav>
                 <ul role='menu'>
-                    <li role='menuitem'><Link to='/'>Codesteez</Link></li>
-                    <li role='menuitem'><Link to='/'>About</Link></li>
+                    <li role='menuitem'><Link className={styles.link} to='/'><Logo /></Link></li>
+                    <li role='menuitem'><Link className={styles.link} to='/'>About</Link></li>
+                    <li className={styles.middle_align} role='menuitem'><ThemeSwitch /></li>
                 </ul>
-                <ThemeSwitch />
             </nav>
         </header>
     )
---
+++ src/components/header/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Logo.module.scss'
+
+const Logo = () => {
+    return(
+        <div className={styles.logo}>
+            Code Steez
+        </div>
+    )
+}
+
+export default Logo
