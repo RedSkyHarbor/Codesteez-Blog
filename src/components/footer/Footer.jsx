9f0e9646d418d1d56a717a784baefa7122e40225
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-03 21:26:36 -0400

    removed broken links in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Footer.module.scss'
 
 const Footer = () => {
     return (
-        <footer >
-            Footer placeholder
+        <footer className={styles.footer}>            
+            <span>Created and maintained by Jesse Breuer.</span> <span>Source code released under the MIT license.</span> <span>Website and documentation licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>.</span>
         </footer>
     )
 }
