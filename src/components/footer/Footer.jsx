8e0a50b8be88ebff83deb6b9d01ddfaddde27cd8
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-03 22:46:09 -0400

    finished basic idea of footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { TwitterFollowButton } from 'react-twitter-embed'
 import styles from './Footer.module.scss'
 
 const Footer = () => {
     return (
-        <footer className={styles.footer}>            
-            <span>Created and maintained by Jesse Breuer.</span> <span>Source code released under the MIT license.</span> <span>Website and documentation licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>.</span>
+        <footer className={styles.footer}>
+            <div className={styles.footer_content}>
+                <TwitterFollowButton screenName={'CodingSteez'}/>
+                <span>Created and maintained by Jesse Breuer.</span> <span>Source code released under the MIT license.</span> <span>Website and documentation licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>.</span>
+            </div>
         </footer>
     )
 }
