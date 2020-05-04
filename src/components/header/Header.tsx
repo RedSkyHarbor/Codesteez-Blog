18264d4f6bec8becd46a0a4b0610c2d3bc9e7d05
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-04 18:43:49 -0400

    completed header for computer view

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.scss'
 
 import ThemeSwitch from './ThemeSwitch'
 import Logo from './Logo'
 
-const Header = () => {
-    return (
-        <header className={styles.header}>
-            <nav>
-                <ul role='menu'>
-                    <li role='menuitem'><Link className={styles.link} to='/'><Logo /></Link></li>
-                    <li role='menuitem'><Link className={styles.link} to='/'>About</Link></li>
-                    <li className={styles.middle_align} role='menuitem'><ThemeSwitch /></li>
-                </ul>
-            </nav>
-        </header>
-    )
+import { ReactComponent as HamburgerIcon} from './hamburger_icon.svg'
+
+class Header extends Component {
+    render() {
+        return (
+            <header className={styles.header}>
+                <nav>
+                    <Logo />
+                    <ul role='menu'>
+                        <li role='menuitem'><Link to='/'>Blog</Link></li>
+                        <li role='menuitem'><Link to='/'>About</Link></li>
+                        <li role='menuitem'><Link to='/'>Contact</Link></li>
+                        <li role='menuitem'><ThemeSwitch /></li>
+                        <li role='menuitem' className={styles.collapse}><HamburgerIcon /></li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
 }
 
 export default Header
