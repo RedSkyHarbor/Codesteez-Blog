073997068fd895a01923ff07d37ff8eaeed2c7a4
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-04 17:44:57 -0400

    Logo is now its own link

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './Logo.module.scss'
 
 const Logo = () => {
     return(
         <div className={styles.logo}>
-            Code Steez
+            <Link to='/'>Code Steez</Link>
         </div>
     )
 }
