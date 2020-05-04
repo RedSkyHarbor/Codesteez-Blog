0f3eb5d1fc9061e340c8090eff35fea3d3dc40a4
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-04 17:45:40 -0400

    cleaned styling in footet

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import styles from './Footer.module.scss'
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div className={styles.footer_content}>
+            <div>
                 <TwitterFollowButton screenName={'CodingSteez'}/>
                 <span>Created and maintained by Jesse Breuer.</span> <span>Source code released under the MIT license.</span> <span>Website and documentation licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>.</span>
             </div>
