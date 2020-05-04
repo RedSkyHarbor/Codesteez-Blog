092c47c8e0aa35ec931d83ac1906d260b91e9438
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-04 19:38:33 -0400

    commented out log that was used for testing

--- src/butter-client.tsx
+++ src/butter-client.tsx
@@ -7,8 +7,9 @@ const BUTTER_API_KEY: string = process.env.REACT_APP_BUTTER_API!
 
 const butter = Butter(BUTTER_API_KEY);
 
-butter.post.list({page: 1, page_size: 10}).then(function(response) {
-    console.log(response)
-})
+// For testing
+//butter.post.list({page: 1, page_size: 10}).then(function(response) {
+//    console.log(response)
+//})
 
 export default butter;
