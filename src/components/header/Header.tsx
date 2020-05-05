a294ad42893964e3987b586009089515d1c1199e
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-04 20:13:06 -0400

    Header is now a functional component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,29 +1,16 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import styles from './Header.module.scss'
 
-import ThemeSwitch from './ThemeSwitch'
 import Logo from './Logo'
+import NavigationMenu from './NavigationMenu'
 
-import { ReactComponent as HamburgerIcon} from './hamburger_icon.svg'
-
-class Header extends Component {
-    render() {
-        return (
-            <header className={styles.header}>
-                <nav>
-                    <Logo />
-                    <ul role='menu'>
-                        <li role='menuitem'><Link to='/'>Blog</Link></li>
-                        <li role='menuitem'><Link to='/'>About</Link></li>
-                        <li role='menuitem'><Link to='/'>Contact</Link></li>
-                        <li role='menuitem'><ThemeSwitch /></li>
-                        <li role='menuitem' className={styles.collapse}><HamburgerIcon /></li>
-                    </ul>
-                </nav>
-            </header>
-        )
-    }
+const Header = () =>{
+    return (
+        <header className={styles.header}>
+            <Logo />
+            <NavigationMenu />
+        </header>
+    )
 }
 
 export default Header
