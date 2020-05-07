73f3b73a631cf2af6ee06068f26b18a409e5d68a
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-06 20:44:22 -0400

    add dependency: emailjs

--- src/components/forms/ContactForm.tsx
+++ src/components/forms/ContactForm.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import emailjs from 'emailjs-com';
+import styles from './ContactForm.module.scss'
 
-const ContactForm = () => {
+export default function ContactForm() {
+
+    function sendEmail(e: any) {
+        e.preventDefault();
+
+        emailjs.sendForm((process.env.REACT_APP_EMAILJS_SERVICE_ID as string), (process.env.REACT_APP_EMAILJS_TEMPLATE_ID as string), e.target, (process.env.REACT_APP_EMAILJS_USER_ID as string))
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
+
     return (
-        <div>
-            Contact Form Placeholder
+        <div className='content'>
+            <form className="contact-form" onSubmit={sendEmail}>
+                <input type="hidden" name="contact_number" />
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+            </form>
         </div>
-    )
+    );
 }
-
-export default ContactForm
--- src/pages/Contactpage.tsx
+++ src/pages/Contactpage.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import Header from '../components/header/Header'
 import ContactForm from '../components/forms/ContactForm'
+import Footer from '../components/footer/Footer'
 
 const ContactPage = () => {
     return (
         <div>
+            <Header />
             <ContactForm />
+            <Footer />
         </div>
     )
 }
