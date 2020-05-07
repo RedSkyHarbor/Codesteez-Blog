f7a69bb12ea08701239e0a72861488df7fcdac8e
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-07 18:24:31 -0400

    add content and styling to about page

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import styles from './About.module.scss'
+
+// @ts-ignore
+import { TwitterFollowButton } from 'react-twitter-embed'
 
 const About = () => {
     return (
         <div className='content'>
-            About component content
+            <div className={styles.about_container}>
+                <h3>Code steez is maintained by Jesse <span role='image' aria-label='B'>🅱️</span>.</h3>
+                <h3>Each post is written by Jesse <span role='image' aria-label='B'>🅱️</span>.</h3>
+                <h3>Give Jesse <span role='image' aria-label='B'>🅱️</span> a follow.</h3>
+            </div>
+            <div className={styles.social_container}>
+                <TwitterFollowButton screenName={'codingsteez'} />
+            </div>
         </div>
     )
 }
