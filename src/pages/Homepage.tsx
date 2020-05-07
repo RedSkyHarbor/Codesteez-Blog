0f9b1436f627a40039f2487a8db32fc885c3c585
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-07 17:22:33 -0400

    homepage is now a functional component

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Helmet } from 'react-helmet'
 
 import Header from '../components/header/Header'
 import PostList from '../components/blog/PostList'
 import Footer from '../components/footer/Footer'
 
-class Homepage extends Component {
-    render() {
-        return (
-            <div>
-                <Helmet><title>Blog | Code Steez</title></Helmet>
-                <Header />
-                <PostList />
-                <Footer />
-            </div>
-        )
-    }
+const Homepage = () => {
+    return (
+        <div>
+            <Helmet><title>Blog | Code Steez</title></Helmet>
+            <Header />
+            <PostList />
+            <Footer />
+        </div>
+    )
 }
 
 export default Homepage
