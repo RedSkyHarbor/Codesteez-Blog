c522f296c9e280cc0313ee4bbe89824632e8d881
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-08 20:47:51 -0400

    Links in navigationmenu are now NavLink to add styling to the link when the link matches the current URL and added that styling called .active in app.scss

--- src/components/header/NavigationMenu.tsx
+++ src/components/header/NavigationMenu.tsx
@@ -1,41 +1,46 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styles from './NavigationMenu.module.scss'
 
 import { ReactComponent as HamburgerIcon} from './hamburger_icon.svg'
 
 
-class NavigationMenu extends Component<{mobile_collapse?: boolean}, any> {
+interface state {
+    isMobileUser: boolean;
+}
+
+interface props {}
+
+class NavigationMenu extends Component<props, state> {
     constructor(props: any){
         super(props)
         this.state = {
-            mobile_collapse: true,
+            isMobileUser: true,
         }
         this.toggleCollapse = this.toggleCollapse.bind(this)
     }
 
     toggleCollapse = () => {
-        this.setState({ mobile_collapse: !this.state.mobile_collapse })
+        this.setState({ isMobileUser: !this.state.isMobileUser })
     }
 
     render() {
-        console.log(this.state.mobile_collapse)
         return (
             <nav>
                 <div className={styles.desktop}>
                     <ul role='menu'>
-                        <li role='menuitem'><Link to='/'>Blog</Link></li>
-                        <li role='menuitem'><Link to='/about'>About</Link></li>
-                        <li role='menuitem'><Link to='/contact'>Contact</Link></li>
+                        <li role='menuitem'><NavLink exact to='/'>Blog</NavLink></li>
+                        <li role='menuitem'><NavLink to='/about'>About</NavLink></li>
+                        <li role='menuitem'><NavLink to='/contact'>Contact</NavLink></li>
                         <li></li>
                     </ul>
                 </div>
 
                 <div className={styles.mobile}>
-                    <ul className={this.state.mobile_collapse ? 'collapsed' : 'expanded'} role='menu'>
-                        <li role='menuitem'><Link to='/'>Blog</Link></li>
-                        <li role='menuitem'><Link to='/about'>About</Link></li>
-                        <li role='menuitem'><Link to='/contact'>Contact</Link></li>
+                    <ul className={this.state.isMobileUser ? 'collapsed' : 'expanded'} role='menu'>
+                        <li role='menuitem'><NavLink exact  to='/'>Blog</NavLink></li>
+                        <li role='menuitem'><NavLink to='/about'>About</NavLink></li>
+                        <li role='menuitem'><NavLink to='/contact'>Contact</NavLink></li>
                     </ul>
                     <HamburgerIcon className={styles.hamburger} onClick={this.toggleCollapse} />
                 </div>
