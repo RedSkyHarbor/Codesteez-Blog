83d93d5c8fe623a7174803f83b3107f066cf33a7
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-09 14:16:07 -0400

    removed emoji B for the letter B to surpress warnings

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -8,9 +8,9 @@ const About = () => {
     return (
         <div className='content'>
             <div className={styles.about_container}>
-                <h3>Code steez is maintained by Jesse <span role='image' aria-label='B'>🅱️</span>.</h3>
-                <h3>Each post is written by Jesse <span role='image' aria-label='B'>🅱️</span>.</h3>
-                <h3>Give Jesse <span role='image' aria-label='B'>🅱️</span> a follow if you could ever be so kind.</h3>
+                <h3>Code steez is maintained by Jesse B.</h3>
+                <h3>Each post is written by Jesse B.</h3>
+                <h3>Give Jesse B a follow if you could ever be so kind.</h3>
             </div>
             <div className={styles.social_container}>
                 <TwitterFollowButton screenName={'codingsteez'} />
