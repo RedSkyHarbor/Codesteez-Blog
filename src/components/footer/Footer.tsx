91d85451d16eca5fe12135ea0117cc35095f0e54
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-11 17:35:38 -0400

    added social links to my footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import styles from './Footer.module.scss'
 
+import { ReactComponent as GithubLogo } from './github_logo.svg'
+import { ReactComponent as TwitterLogo } from './twitter_logo.svg'
+
 import Logo from '../logo/Logo'
 import ScrollToTop from '../scroll/ScrollToTop'
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div>
-                <ScrollToTop />
-                <Logo />
-                <p>Code Steez - A high quality, in depth programming blog.</p>
+            <ScrollToTop />
+            <Logo />
+            <p>Code Steez - A high quality, in depth programming blog.</p>
+            <div className={styles.social_links}>
+                <a rel="noopener noreferrer" target="_blank" href='https://twitter.com/CodingSteez'><GithubLogo className={styles.github_logo} /></a>
+                <a rel="noopener noreferrer" target="_blank" href='https://github.com/RedSkyHarbor'><TwitterLogo className={styles.twitter_logo} /></a>
             </div>
         </footer>
     )
