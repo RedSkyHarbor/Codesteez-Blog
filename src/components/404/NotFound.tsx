452bb573421a827636298540446606fc971d0a02
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-04 16:14:25 -0400

    added link back to homepage

--- src/components/404/NotFound.tsx
+++ src/components/404/NotFound.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 import styles from './NotFound.module.scss'
 
 const NotFound = () => {
     return ( 
         <div className={styles.not_found}>
-            Not found
+            <h1 className={styles.oops}>Oops!</h1>
+            <h2>This page wasn't found</h2>
+            <NavLink to='/'>Let's go back home</NavLink>
         </div>
 
     )
