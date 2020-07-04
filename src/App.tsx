bce80026a6a6181f82ff50869d686710d364d51e
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-07-04 16:55:36 -0400

    fixed bug - added switch to router so it doesnt match several url patterns at once

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import './App.scss';
 import './fonts/Playfair_Display/PlayfairDisplay.ttf'
@@ -13,12 +13,13 @@ import NotFoundPage from './pages/NotFoundPage'
 function App() {
   return (
     <Router>
-      <Route path='/' exact component={HomePage} />
-      <Route path='/blog/posts/:slug' component={PostPage} />
-      <Route path='/contact' component={ContactPage} />
-      <Route path='/about' component={AboutPage} />
-      <Route path='/404' component={NotFoundPage} />
-      <Redirect to='/404' />
+      <Switch>
+        <Route path='/' exact component={HomePage} />
+        <Route path='/blog/posts/:slug' component={PostPage} />
+        <Route path='/contact' component={ContactPage} />
+        <Route path='/about' component={AboutPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </Router>
   );
 }
